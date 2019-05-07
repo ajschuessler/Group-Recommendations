53532580903316b7fc72f3571f37cfd777c112f2
Andrew Schuessler <ajschuessler@gmail.com>
2019-05-07 18:22:01 -0400

    updated to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     app: ['babel-polyfill', `${SRC_DIR}/index.jsx`]
   },
